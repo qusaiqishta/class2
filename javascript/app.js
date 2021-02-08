02c5877a6b4786b3b4145df9059be272193a5070
qusaiqishta <qeshtaqusai0@gmail.com>
2021-02-08 15:16:59 +0200

    finish

---
+++ javascript/app.js
@@ -0,0 +1,87 @@
+
+let Name = prompt('what\'s your name?');
+console.log(Name);
+alert(Name + " is a very nice name");
+
+let happiness = prompt('are you happy?');
+console.log(happiness);
+
+switch (happiness.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        alert('that\'s great !');
+        break;
+
+    case 'NO':
+    case 'N':
+        alert("do what ever it takes to be happy , it\'s a very important thing");
+        break;
+}
+
+let end = prompt('Do you know when it\'s going to be the end of the world? ');
+console.log(end);
+
+switch (end.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        alert('then i guess you already prepared yourself!');
+        break;
+
+    case 'NO':
+    case 'N':
+        alert("you have to be prepared as soon as possible because it\s very close");
+        break;
+}
+
+let hole = prompt('Do yoy know what is the black hole?');
+console.log(hole);
+
+switch (hole.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        alert('so you has to make youself not to drop into in');
+        break;
+
+    case 'NO':
+    case 'N':
+        alert("it\'s a very dense energy hole that have the capacity to consume every thing even the light!");
+        break;
+}
+
+let rich = prompt('Do you think you are rich?');
+console.log(rich);
+switch (rich.toUpperCase()) {
+    case 'YES':
+    case 'Y':
+        alert('so why you are here in the jordan!');
+        break;
+
+    case 'NO':
+    case 'N':
+        alert("i hope you be");
+        break;
+}
+
+let lonely = prompt('Do you feel lonely?');
+console.log(lonely);
+
+
+switch (lonely.toUpperCase()) {
+    case YES:
+    case Y:
+        alert('that\'s better than to be with fake people');
+        break;
+
+    case NO:
+    case N:
+        alert("okay!");
+        break;
+}
+
+
+
+
+
+
+
+
